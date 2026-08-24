22539cc2d045794584d3cceff678823a160b3991
Nikolai Matiushev <egao1980@gmail.com>
2026-08-24 22:37:57 +0100

    Route A2A gRPC through rpc-protocol-grpc.
    Do not call grpc-protocol from the agent-wire backend.

--- src/package.lisp
+++ src/package.lisp
@@ -2,6 +2,8 @@
   (:use #:cl)
   (:export #:grpc-a2a-backend
            #:make-grpc-a2a-backend
-           #:use-grpc-a2a-backend))
+           #:use-grpc-a2a-backend
+           #:backend-transport
+           #:backend-target))
 
 (in-package #:a2a-backend-grpc)
